ef09f55e54dbf9fccf9f0c9ff416aafbb0ba3fe2
Len Bucchino <lb4@bitfly.org>
2022-05-22 17:27:07 -0500

    creating schema

--- stocktrack-api/create_schema.sql
+++ stocktrack-api/create_schema.sql
@@ -7,3 +7,43 @@ create table test
     name varchar(255),
     PRIMARY KEY (id)
 );
+
+create table alerts
+(
+    id   bigint NOT NULL AUTO_INCREMENT,
+    ticker varchar(10),
+    alertType int,
+    alertValue double,
+    activeFlag tinyint,
+    PRIMARY KEY (id)
+);
+
+create table alertHistory
+(
+    id   bigint NOT NULL AUTO_INCREMENT,
+    alertId bigint,
+    alertDate datetime,
+    stockDataId bigint,
+    PRIMARY KEY (id)
+);
+
+create table stockData
+(
+    id    bigint NOT NULL AUTO_INCREMENT,
+    ticker varchar(10),
+    timePulled datetime,
+    price double,
+    dollarChange double,
+    percentChange double,
+    PRIMARY KEY (id)
+);
+
+create table properties
+(
+    theKey varchar(255),
+    theValue varchar(255),
+    primary key(theKey)
+);
+
+
+
